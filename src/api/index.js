54323e7a5069e9fb33b1e5ece4373870e8987f27
pranjal.trvd <pranjaltrvd12@gmail.com>
2024-04-27 16:31:58 +0530

    Initial Commit

---
+++ src/api/index.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+export const checkStatus = async (token) => {
+  const options = {
+    method: "GET",
+    url: 'https://judge0-ce.p.rapidapi.com/submissions/' + token,
+    params: { base64_encoded: "true", fields: "*" },
+    headers: {
+      "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+  try {
+    let response = await axios.request(options);
+    let statusId = response.data.status?.id;
+
+    if (statusId === 1 || statusId === 2) {
+      //  processing --> so run again the same token after 2s
+      return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          try {
+            const result = await checkStatus(token);
+            resolve(result);
+          } catch (err) {
+            reject(err);
+          }
+        }, 2000);
+      });
+    } else {
+      const { data } = response;
+
+      console.log(data);
+
+      return { success: true, data };
+    }
+  } catch (err) {
+    return { success: false, err };
+  }
+};
+
+export const submitCode = async (formData) => {
+  const options = {
+    method: "POST",
+    url: 'https://judge0-ce.p.rapidapi.com/submissions',
+    params: { base64_encoded: "true", fields: "*" },
+    headers: {
+      "content-type": "application/json",
+      "Content-Type": "application/json",
+      "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    },
+    data: formData,
+  };
+
+  try {
+    const { data } = await axios.request(options);
+    console.log(data);
+
+    return { success: true, data };
+  } catch (err) {
+    return { success: false, err }
+  }
+}
